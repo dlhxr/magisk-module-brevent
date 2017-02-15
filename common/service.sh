057d21d2886aa4ddd09ec9aef2289353da9ac2a9
Simon <simonsmh@gmail.com>
2017-02-15 23:26:50 +0800

    try it

--- common/service.sh
+++ common/service.sh
@@ -2,67 +2,13 @@
 # Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
-
 restartadb(){
-/system/bin/setprop service.adb.tcp.port $1
+/system/bin/setprop service.adb.tcp.port 5555
 /system/bin/stop adbd
 /system/bin/start adbd
 }
-# This script will be executed in late_start service mode
-# More info in the main Magisk thread
-
-#{
 restartadb 5555
-### check environment
-#uid=`id -u`
-#if [ x"$uid" != x"2000" ]; then
-#    echo "cannot be run as non-shell" >&2
-#    exit 1
-#fi
-#context=`id -Z`
-#if [ x"$context" != x"context=u:r:shell:s0" ];then
-#    echo "cannot be run as non-shell" >&2
-#    exit 1
-#fi
-#apk=`pm path me.piebridge.brevent | tr -d '\r\n' | cut -b 9-`
-#if [ x"$apk" == x"" ]; then
-#    echo "cannot find brevent" >&2
-#    exit 1
-#else
-### kill exist server pid
-#export IFS=" /";
-#netstat -tlnp 2>/dev/null | grep 59526 | while read pr rq sq la fa st pid pn; do
-#if [ ! -d /proc/$pid ]; then
-#echo "cannot find pid $pid" >&2
-#exit 2
-#fi
-#echo "kill $pid"
-#kill $pid
-#if [ ! -d /proc/$pid ]; then
-#echo "cannot kill $pid" >&2
-#exit 2
-#fi
-#done
-$MODDIR/system/bin/kadb kill-server
-$MODDIR/system/bin/kadb shell <<< {
-export CLASSPATH=`pm path me.piebridge.brevent | tr -d '\r\n' | cut -b 9-` 
-exec app_process /system/bin me.piebridge.brevent.server.Brevent "$@" & 
-} &
-kill $!
-#$MODDIR/system/bin/kadb kill-server
-# checking part
-#    echo -n "checking..."
-#    for x in 1 2 3 4 5 6 7 8 9 10; do
-#        sleep 1
-#        if netstat -tlnp 2>/dev/null | grep -q 59526; then
-#            echo "successfully" >&2
-#            exit 0
-#        fi
-#        echo -n "."
-#    done
-#    echo ""
-#    echo "cannot listen port, please report bug" >&2
-#    exit 3
-#fi
+export HOME=/sdcard
+$MODDIR/system/bin/adb shell <<< 'export CLASSPATH=`pm path me.piebridge.brevent | tr -d '\r\n' | cut -b 9-`  && exec app_process /system/bin me.piebridge.brevent.server.Brevent "$@" &  ' &
+$MODDIR/system/bin/adb kill-server
 restartadb 0
-#} > $MODDIR/log.txt &
