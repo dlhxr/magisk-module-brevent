7a2c4d6fb0aadf1285d399a8984ecb96c5352b1a
Simon <simonsmh@gmail.com>
2017-02-16 14:45:47 +0800

    refine xcommand

--- common/service.sh
+++ common/service.sh
@@ -1,14 +1,14 @@
 #!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
-restartadb(){
-/system/bin/setprop service.adb.tcp.port 5555
-/system/bin/stop adbd
-/system/bin/start adbd
-}
-restartadb 5555
-export HOME=/sdcard
-$MODDIR/system/bin/adb shell <<< 'export CLASSPATH=`pm path me.piebridge.brevent | tr -d '\r\n' | cut -b 9-`  && exec app_process /system/bin me.piebridge.brevent.server.Brevent "$@" &  ' &
-$MODDIR/system/bin/adb kill-server
-restartadb 0
+# idea from manhong2112
+HOME=/sdcard
+adb kill-server
+setprop service.adb.tcp.port 5555
+stop adbd && start adbd
+setprop service.adb.tcp.port -1
+# Both run well under shell.
+## Original code running experience.
+adb shell "sh `pm path me.piebridge.brevent | tr -d '\r\n' | cut -b 9-40`/lib/arm/libbootstrap.so" &
+## From the core part of the libbootstrap.so
+adb shell "export CLASSPATH=`pm path me.piebridge.brevent | tr -d '\r\n' | cut -b 9-` ;exec app_process /system/bin me.piebridge.brevent.server.Brevent $@" &
+# None of them working in magisk.
