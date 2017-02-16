db5cb4e71bf7d0436094410d86ea6bcb828cde1b
Simon <simonsmh@gmail.com>
2017-02-16 22:30:06 +0800

    drop support of modified command

--- common/service.sh
+++ common/service.sh
@@ -6,9 +6,5 @@ adb kill-server
 setprop service.adb.tcp.port 5555
 stop adbd && start adbd
 setprop service.adb.tcp.port -1
-# Both run well under shell.
-## Original code running experience.
+## It is able to run in shell only.
 adb shell "sh `pm path me.piebridge.brevent | tr -d '\r\n' | cut -b 9-40`/lib/arm/libbootstrap.so" &
-## From the core part of the libbootstrap.so
-adb shell "export CLASSPATH=`pm path me.piebridge.brevent | tr -d '\r\n' | cut -b 9-` ;exec app_process /system/bin me.piebridge.brevent.server.Brevent $@" &
-# None of them working in magisk.
