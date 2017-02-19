fc4d6f15126848b32b4d5571650b45381611ee51
Simon <simonsmh@gmail.com>
2017-02-19 15:39:02 +0800

    q

--- common/service.sh
+++ common/service.sh
@@ -1,11 +1,14 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 # idea from manhong2112
-HOME=/sdcard
+export HOME=/sdcard
+export CLASSPATH=`pm path me.piebridge.brevent | cut -b 9-40`/lib/arm/libloader.so
 adb kill-server
 setprop service.adb.tcp.port 5555
 stop adbd && start adbd
 setprop service.adb.tcp.port -1
-## It is able to run in shell only.
 adb shell "sh `pm path me.piebridge.brevent | tr -d '\r\n' | cut -b 9-40`/lib/arm/libbootstrap.so" &
-# su -p shell -c "export CLASSPATH=`pm path me.piebridge.brevent | cut -b 9-40`/lib/arm/libloader.so && /system/bin/app_process32 /system/bin --nice-name=brevent_server me.piebridge.brevent.loader.Brevent $@ &"
+
+#context error
+#su shell -p -cn u:r:shell:s0 -c "/system/bin/app_process32 /system/bin --nice-name=brevent_server me.piebridge.brevent.loader.Brevent \"$@\" &"
+
