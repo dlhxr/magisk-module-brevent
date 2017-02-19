b035ba457615328e4000ef391f6de5a3e5de8ac4
Simon <simonsmh@gmail.com>
2017-02-19 12:25:27 +0800

    new method

--- common/service.sh
+++ common/service.sh
@@ -8,3 +8,4 @@ stop adbd && start adbd
 setprop service.adb.tcp.port -1
 ## It is able to run in shell only.
 adb shell "sh `pm path me.piebridge.brevent | tr -d '\r\n' | cut -b 9-40`/lib/arm/libbootstrap.so" &
+# su -p shell -c "export CLASSPATH=`pm path me.piebridge.brevent | cut -b 9-40`/lib/arm/libloader.so && /system/bin/app_process32 /system/bin --nice-name=brevent_server me.piebridge.brevent.loader.Brevent $@ &"
