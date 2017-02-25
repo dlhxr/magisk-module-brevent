5dd14740d78220a97f14410383d3bfdd64059f9c
Simon <simonsmh@gmail.com>
2017-02-25 09:46:58 +0800

    Add architecture check

--- common/service.sh
+++ common/service.sh
@@ -1,5 +1,12 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
+if getprop ro.product.cpu.abilist | grep -q x86; then
+    abi=x86
+else
+    abi=arm
+fi
+
 sleep 20
-`pm path me.piebridge.brevent | tr -d '\r\n' | cut -b 9-40`/lib/arm/libbootstrap.so
+
+`pm path me.piebridge.brevent | tr -d '\r\n' | cut -b 9-40`/lib/$abi/libbootstrap.so
