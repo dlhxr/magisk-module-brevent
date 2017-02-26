be2080acf0c78bc4fdc39c7634457fe1f3a7a05a
Simon <simonsmh@gmail.com>
2017-02-26 13:55:44 +0800

    q

--- common/service.sh
+++ common/service.sh
@@ -1,4 +1,4 @@
-#!/system/bin/sh
+#!/system/bin/sh -x
 MODDIR=${0%/*}
 if getprop ro.product.cpu.abilist | grep -q x86
 then
@@ -6,6 +6,6 @@ abi=x86
 else
 abi=arm
 fi
-exec `pm path me.piebridge.brevent | cut -b 9-40`/lib/$abi/libbootstrap.so
-
-#su shell -p -cn u:r:shell:s0 -c "/system/bin/app_process32 /system/bin --nice-name=brevent_server me.piebridge.brevent.loader.Brevent \"$@\" &"
+rm -f /cache/Brevent.log
+su -c `pm path me.piebridge.brevent | cut -b 9-40`/lib/$abi/libbootstrap.so &> /cache/Brevent.log
+#su shell -p -cn u:r:shell:s0 -c "`pm path me.piebridge.brevent | cut -b 9-40`/lib/$abi/libbootstrap.so"
